2c434841302c458d0b9972acb68acd48d844dcae
Wataru MIYAGUNI <gonngo@gmail.com>
2013-06-24 13:12:54 +0900

    しばらく animation gif はあきらめて静止画にもどす、で変更しわすれてた所…

--- lib/gnawrnip/ext/capybara/session.rb
+++ lib/gnawrnip/ext/capybara/session.rb
@@ -8,13 +8,13 @@ module Capybara
       :evaluate_script, :visit
     ]
 
-    SAVE_SCREENSHOT_METHODS.each do |method|
-      alias_method "after_hook_#{method}".to_sym, method
-
-      define_method method do |*args, &block|
-        send("after_hook_#{method}", *args, &block)
-        Gnawrnip::Animation.add_frame
-      end
-    end
+    # SAVE_SCREENSHOT_METHODS.each do |method|
+    #   alias_method "after_hook_#{method}".to_sym, method
+    #
+    #   define_method method do |*args, &block|
+    #     send("after_hook_#{method}", *args, &block)
+    #     Gnawrnip::Animation.add_frame
+    #   end
+    # end
   end
 end
