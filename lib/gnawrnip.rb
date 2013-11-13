e09bdb48a535c3ccf599ee2faf9678925250c795
Wataru MIYAGUNI <gonngo@gmail.com>
2013-11-13 22:09:26 +0900

    Delete rmagick mode

--- lib/gnawrnip.rb
+++ lib/gnawrnip.rb
@@ -1,11 +1,10 @@
 require "gnawrnip/version"
 require 'gnawrnip/rspec'
 require 'gnawrnip/photographer'
-require 'gnawrnip/step_screenshot'
+require 'gnawrnip/publisher'
 
 module Gnawrnip
   class << self
-    attr_accessor :publisher_driver
     attr_accessor :frame_interval
     attr_accessor :frame_size
     attr_accessor :make_animation
@@ -16,7 +15,7 @@ module Gnawrnip
 
     def ready!
       require 'gnawrnip/ext/capybara/session' if animation?
-      publisher
+      require 'gnawrnip/step_screenshot'
     end
 
     def animation?
@@ -24,24 +23,20 @@ module Gnawrnip
     end
 
     def photographer
-      @photographer ||= Gnawrnip::Photographer.new
+      @photographer ||= Photographer.new
     end
 
     def publisher
-      @publisher ||= case publisher_driver
-                     when :rmagick
-                       require 'gnawrnip/publisher/rmagick'
-                       @publisher = Publisher::RMagick.new
-                     else # :js
-                       require 'gnawrnip/publisher/js'
-                       @publisher = Publisher::JS.new
-                     end
+      @publisher ||= Publisher.new
+    end
+
+    def publisher_driver=(driver)
+      warn "DEPRECATED: `publisher_driver` option is deprecated (not used)."
     end
   end
 end
 
 Gnawrnip.configure do |c|
-  c.publisher_driver = :js
   c.frame_interval   = 1000
   c.frame_size       = nil
   c.make_animation   = true
