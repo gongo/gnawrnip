53526fd88a55c615e7fd3d0b6b0d31a30fac0623
Wataru MIYAGUNI <gonngo@gmail.com>
2014-09-30 21:33:39 +0900

    Reduced the action of the timing to take a screenshot GH-19

--- lib/gnawrnip/ext/capybara/session.rb
+++ lib/gnawrnip/ext/capybara/session.rb
@@ -2,8 +2,23 @@ require 'capybara/session'
 
 module Capybara
   class Session
-    SAVE_SCREENSHOT_METHODS = NODE_METHODS + [
-      :visit, :has_title?, :has_no_title?, :go_back, :go_forward
+    SAVE_SCREENSHOT_METHODS = [
+      :click_button,
+      :click_link,
+      :click_link_or_button,
+      :click_on,
+      :fill_in,
+      :choose,
+      :check,
+      :uncheck,
+      :select,
+      :unselect,
+      :attach_file,
+      :execute_script,
+      :go_back,
+      :go_forward,
+      :switch_to_window,
+      :visit
     ]
 
     SAVE_SCREENSHOT_METHODS.each do |method|
--- lib/gnawrnip/step_screenshot.rb
+++ lib/gnawrnip/step_screenshot.rb
@@ -1,4 +1,3 @@
-require 'base64'
 require 'turnip_formatter/step_template/exception'
 require 'gnawrnip/developer'
 
