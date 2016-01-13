381ac46c1d775d95cefc73b22b4dd764dd827cca
Wataru MIYAGUNI <gonngo@gmail.com>
2016-01-13 20:32:46 +0900

    Remove `get current_example` method for RSpec 2.x

--- lib/gnawrnip/rspec.rb
+++ lib/gnawrnip/rspec.rb
@@ -5,18 +5,14 @@ RSpec.configure do |config|
     Gnawrnip.ready!
   end
 
-  # https://github.com/jnicklas/capybara/blob/master/lib/capybara/rspec.rb
-  fetch_current_example = RSpec.respond_to?(:current_example) ?
-    proc { RSpec.current_example } : proc { |context| context.example }
-
   config.before(:each, turnip: true) do
-    example = fetch_current_example.call(self)
+    example = RSpec.current_example
     Gnawrnip.photographer.reset!
     example.metadata[:gnawrnip] = {}
   end
 
   config.after(:each, turnip: true) do
-    example = fetch_current_example.call(self)
+    example = RSpec.current_example
 
     if example.exception
       Gnawrnip.photographer.take_shot
